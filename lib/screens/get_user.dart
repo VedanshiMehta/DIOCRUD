e734d399937b913e3cb0a5d4141b498954e29cdc
Vedanshi.Mehta <vedanshi.mehta@1rivet.com>
2023-07-11 13:03:45 +0530

    inital commit

---
+++ lib/screens/get_user.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GetUser extends StatefulWidget {
+  const GetUser({super.key});
+
+  @override
+  State<GetUser> createState() => _GetUserState();
+}
+
+class _GetUserState extends State<GetUser> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
